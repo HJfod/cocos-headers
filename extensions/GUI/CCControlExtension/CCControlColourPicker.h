94cf68a835722536ebf250ef07e26942aeb9b0a1
HJfod <60038575+HJfod@users.noreply.github.com>
2021-05-26 19:33:04 +0300

    fix

--- extensions/GUI/CCControlExtension/CCControlColourPicker.h
+++ extensions/GUI/CCControlExtension/CCControlColourPicker.h
@@ -75,7 +75,7 @@ public:
         CCSprite* getColorTarget();
         ColorPickerDelegate* getDelegate();
 
-        void setColorValue(ccColor3B);
+        void setColorValue(_ccColor3B const&);
         void setColorTarget(CCSprite*);
         void setDelegate(ColorPickerDelegate*);
     )
