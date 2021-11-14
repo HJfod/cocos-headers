4376e41e16b62027de442f0863c14d35142b0c22
hjfod <dreadrollmusic@gmail.com>
2021-11-15 00:25:10 +0200

    CCEGLView funcs

--- cocos2dx/platform/win32/CCEGLView.h
+++ cocos2dx/platform/win32/CCEGLView.h
@@ -132,6 +132,21 @@ protected:
         bool m_bShouldHideCursor;
         bool m_bShouldCallGLFinish;
     )
+
+    RT_ADD(
+        void onGLFWCharCallback(GLFWwindow* window, unsigned int entered);
+        void onGLFWCursorEnterFunCallback(GLFWwindow* window, int entered);
+        void onGLFWDeviceChangeFunCallback(GLFWwindow* window);
+        void onGLFWError(int code, const char* description);
+        void onGLFWframebuffersize(GLFWwindow* window, int width, int height);
+        void onGLFWMouseMoveCallBack(GLFWwindow* window, double x, double y);
+        void onGLFWMouseCallBack(GLFWwindow* window, int button, int action, int mods);
+        void onGLFWKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+        void onGLFWMouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+        void onGLFWWindowIconifyFunCallback(GLFWwindow* window, int iconified);
+        void onGLFWWindowPosCallback(GLFWwindow* window, int x, int y);
+        void onGLFWWindowSizeFunCallback(GLFWwindow* window, int width, int height);
+    )
 };
 
 NS_CC_END
